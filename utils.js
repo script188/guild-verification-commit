f9276b54dac0f62fc6323a9f645e30091d3fab56
script188 <camigraves924@gmail.com>
2026-03-27 22:58:39 +0100

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T21:58:35.270Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-27T21:58:38.791Z
+Commit: Add multi-language support
