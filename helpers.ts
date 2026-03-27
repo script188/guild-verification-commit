2df0e4590444946d007d8aa7254703b9c27c4d7e
script188 <camigraves924@gmail.com>
2026-03-27 22:56:33 +0100

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T21:56:07.898Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-27T21:56:34.276Z
+Commit: Resolve timezone issues in date handling
