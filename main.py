642d868fa19ee30e40ed1bf1867d4a88fce82e30
script188 <camigraves924@gmail.com>
2026-03-27 22:58:17 +0100

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T21:58:14.674Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-27T21:58:17.448Z
+Commit: Fix memory leak in event handlers
