b3af1d79d019060a3afbbdd3c118a07f9886a3d0
script188 <camigraves924@gmail.com>
2026-03-27 23:00:30 +0100

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T22:00:21.172Z
-Commit: Add multi-language support
+Timestamp: 2026-03-27T22:00:30.525Z
+Commit: Resolve timezone issues in date handling
