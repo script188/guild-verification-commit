1a16c54800bded63d7f65d71f23148a2d40f44f7
script188 <camigraves924@gmail.com>
2026-03-27 23:00:47 +0100

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T22:00:30.525Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-27T22:00:47.611Z
+Commit: Patch API endpoint error handling
