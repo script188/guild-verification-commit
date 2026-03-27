040561300f5c6433614c2d8a3e07adfb717881e0
script188 <camigraves924@gmail.com>
2026-03-27 22:58:14 +0100

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T21:56:04.934Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-27T21:58:14.674Z
+Commit: Fix broken links in documentation
